3269a76c9295f68d55b443950ede4f22c8807a1a
Rajkumar <drajkumar01234@gmail.com>
2025-01-14 19:19:30 +0530

    Implement video fetching with pagination, sorting, and user details; add validation for video creation

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,25 +1,122 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Video} from "../models/video.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import mongoose, { isValidObjectId } from "mongoose"
+import { Video } from "../models/video.model.js"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
+
+    const video = await Video.aggregate([
+        {
+            $match: {
+                $or: [
+                    { title: { $regex: query, $options: 'i' } },
+                    { description: { $regex: query, $options: 'i' } }
+                ]
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "createdBy"
+            },
+        },
+        {
+            $unwind: "$createdBy"
+        },
+        {
+            $project: {
+                thumbnail: 1,
+                videoFile: 1,
+                title: 1,
+                description: 1,
+                duration: 1,
+                createdBy: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                }
+            }
+        }
+        ,
+        {
+            $sort: {
+                [sortBy]: sortType === "asc" ? 1 : -1
+            }
+        },
+        {
+            $skip: (page - 1) * limit
+        },
+        {
+            $limit: parseInt(limit)
+        },
+    ])
+
+    return res.status(200).json(new ApiResponse(
+        200,
+        "Videos fetched successfully",
+        video
+    ))
+
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
+    const { title, description } = req.body
     // TODO: get video, upload to cloudinary, create video
+    if (!title || !description) {
+        throw new ApiError(400, "Title and description are required")
+    }
+    const videoFileLocalPath = req.files?.videoFile[0]?.path
+
+    if (!videoFileLocalPath) {
+        throw new ApiError(400, "Video is required")
+    }
+
+    const videoFile = await uploadOnCloudinary(videoFileLocalPath)
+    if (!videoFile.url) {
+        throw new ApiError(400, "Video upload failed")
+    }
+    const thumbnailLocalPath = req.files?.thumbnail[0]?.path
+
+    if (!thumbnailLocalPath) {
+        throw new ApiError(400, "Thumbnail is required")
+    }
+
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+    if (!thumbnail.url) {
+        throw new ApiError(400, "Thumbnail upload failed")
+    }
+    const video = await Video.create({
+        videoFile: videoFile.url,
+        thumbnail: thumbnail.url,
+        title,
+        description,
+        duration: videoFile.duration,
+        owner: req.user._id
+    })
+
+    if (!video) {
+        throw new ApiError(400, "Video creation failed")
+    }
+
+    return res.status(201).json(new ApiResponse(
+        201,
+        "Video created successfully",
+        video
+    ))
+
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
-    //TODO: get video by id
+    
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
