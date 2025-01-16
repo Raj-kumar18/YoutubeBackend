e4f7716417b9f3bcd05dd81c6856ec8a9b5f8ca4
Rajkumar <drajkumar01234@gmail.com>
2025-01-16 14:27:22 +0530

    Add subscription toggle functionality with channel validation and error handling

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,19 +1,57 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {User} from "../models/user.model.js"
+import mongoose, { isValidObjectId } from "mongoose"
+import { User } from "../models/user.model.js"
 import { Subscription } from "../models/subscription.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-    // TODO: toggle subscription
+    const { channelId } = req.params;
+
+    // Validate the channel ID
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel id");
+    }
+
+    // Check if the channel exists
+    const channel = await User.findById(channelId);
+    if (!channel) {
+        throw new ApiError(404, "Channel not found");
+    }
+
+    // Check if a subscription exists
+    const subscription = await Subscription.findOne({
+        subscriber: req.user._id,
+        channel: channelId,
+    });
+
+    if (subscription) {
+        // Unsubscribe: delete the existing subscription
+        await Subscription.findByIdAndDelete(subscription._id);
+        res.status(200).json({
+            success: true,
+            message: "Unsubscribed successfully",
+            data: {},
+        });
+    } else {
+        // Subscribe: create a new subscription
+        await Subscription.create({
+            subscriber: req.user._id,
+            channel: channelId,
+        });
+        res.status(200).json({
+            success: true,
+            message: "Subscribed successfully",
+            data: {},
+        });
+    }
+
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+    const { channelId } = req.params
 })
 
 // controller to return channel list to which user has subscribed
