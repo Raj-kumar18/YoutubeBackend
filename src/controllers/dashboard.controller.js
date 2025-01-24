797b1527e4c508aa9bac4727798d68b71d6a0825
Rajkumar <drajkumar01234@gmail.com>
2025-01-24 13:39:18 +0530

    Implement video retrieval for channel with user ID validation

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -136,6 +136,18 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const userId = req.user._id
+    if(!userId){
+        throw new ApiError(400, "User ID is required")
+    }
+
+    const videos = await Video.find({ owner: userId })
+
+    return res.status(200).json(new ApiResponse(
+        200,
+        videos,
+        "Channel videos fetched successfully",
+    ))
 })
 
 export {
